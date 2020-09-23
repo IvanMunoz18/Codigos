849b5d6f4326fd288563ae906ed066cf4199250f
IvanMunoz18 <18240176@leon.tecnm.mx>
2020-09-23 08:54:02 -0500

    code

---
+++ InterfazVista.java
@@ -0,0 +1,12 @@
+
+package programa1;
+public interface  InterfazVista {
+void setControlador ( Controlador c) ; 
+public String getFileName () ; 
+public String getText () ; 
+public void setText ( String s) ; 
+static final String ABRIR =" Abrir ";
+static final String ENCRYPT =" encrypt ";
+static final String DECRYPT =" decrypt ";
+static final String GUARDAR =" Guardar ";
+}
